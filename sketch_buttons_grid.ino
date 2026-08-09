d4ca607c728bd4fcdf0164eb1ac9f21cb9a209f3
Karollo6767 <karol.stanek12@gmail.com>
2026-08-09 11:54:02 +0200

    Pull repo to update local repo

--- sketch_buttons_grid.ino
+++ sketch_buttons_grid.ino
@@ -1,9 +1,34 @@
+// Nessesary implementations
+// #include <Wire.h>
+// #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+const byte ROWS = 4;
+const byte COLUMNS = 4;
+
+char keyMap[ROWS][COLUMNS] = {
+  {"1", "2", "3", "*"};
+  {"4", "5", "6", "/"};
+  {"7", "8", "9", "+"};
+  {"0", ".", "=", "-"};
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  // pinMode(row1, INPUT_PULLUP);
+  // pinMode(row2, INPUT_PULLUP);
+  // pinMode(row3, INPUT_PULLUP);
+  // pinMode(row4, INPUT_PULLUP);
+  // pinMode(column1, INPUT_PULLUP);
+  // pinMode(column2, INPUT_PULLUP);
+  // pinMode(column3, INPUT_PULLUP);
+  // pinMode(column4, INPUT_PULLUP);
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
 
 }
