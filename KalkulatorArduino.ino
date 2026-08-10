0cb9077660664d10108f9db62f2bc3704b047e3d
Karollo6767 <karol.stanek12@gmail.com>
2026-08-10 14:13:24 +0200

    Add lcd code and ability to input nums

---
+++ KalkulatorArduino.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+
+const byte ROWS = 4;  //four rows
+const byte COLS = 4;  //four columns
+//define the cymbols on the buttons of the keypads
+char keyBinds[ROWS][COLS] = {
+  { '7', '8', '9', '/' },
+  { '4', '5', '6', '*' },
+  { '1', '2', '3', '-' },
+  { '0', '.', '=', '+' }
+};
+
+byte rowPins[ROWS] = { 5, 4, 3, 2 };  //connect to the row pinouts of the keypad
+byte colPins[COLS] = { 9, 8, 7, 6 };  //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad keypad = Keypad(makeKeymap(keyBinds), rowPins, colPins, ROWS, COLS);
+//initialize lcd lcd
+LiquidCrystal_I2C lcd(0x27, 2, 16);
+String inputText = "";  //simple 'box' for the characters
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    Serial.print(key);
+    lcd.print(key);
+  }
+}
