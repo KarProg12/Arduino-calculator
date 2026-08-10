8fbe139b420a6e8b71388a44f980cbce725c0722
Karollo6767 <karol.stanek12@gmail.com>
2026-08-10 21:55:08 +0200

    Correct the typo in comment on line 44

--- ArduinoCalculator.ino
+++ ArduinoCalculator.ino
@@ -1,9 +1,9 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
-
 const byte ROWS = 4;  //four rows
 const byte COLS = 4;  //four columns
+
 //define the cymbols on the buttons of the keypads
 char keyBinds[ROWS][COLS] = {
   { '7', '8', '9', '/' },
@@ -17,13 +17,17 @@ byte colPins[COLS] = { 9, 8, 7, 6 };  //connect to the column pinouts of the key
 
 //initialize an instance of class NewKeypad
 Keypad keypad = Keypad(makeKeymap(keyBinds), rowPins, colPins, ROWS, COLS);
+
 //initialize lcd lcd
 LiquidCrystal_I2C lcd(0x27, 2, 16);
+
 //numbers of equation in String variable
 String sNum1 = "";
-bool secondNumber = false; 
+String mathOperator = "";
 String sNum2 = "";
-//variables where the String variables are converted to float 
+bool secondNumber = false;
+
+//variables where the String variables are converted to float
 float num1 = 0;
 float num2 = 0;
 float equation = 0;
@@ -37,13 +41,12 @@ void setup() {
 
 void loop() {
   char key = keypad.getKey();
-  //checks if any key on keypad has been clicked if not than tohing happens it just returns NO_KEY
+  //checks if any key on keypad has been clicked if not than nothing happens it just returns NO_KEY
   if (key) {
-    lcd.print(key);    
+    lcd.print(key);
     if (key == 'C') {
       Serial.println(key);
       lcd.clear();
     }
-    
   }
 }
